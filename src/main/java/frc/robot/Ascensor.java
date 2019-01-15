3107a30c1302631d1fbc3fa7ab6b6a86c95b1516
K2SO <varunchari@yahoo.com>
2019-01-15 16:53:30 -0500

    Finished setting height and speed methods, with lower/upper bounds

--- src/main/java/frc/robot/Ascensor.java
+++ src/main/java/frc/robot/Ascensor.java
@@ -20,29 +20,49 @@ public class Ascensor {
 	private Ascensor() {
 	}
 	
+	/**
+	 * Sets mast up/down speed, automatically keeps mast within bounds
+	 * @param speed
+	 */
 	public void setSpeed(double speed) {
+		double currentHeight = height();
+		if ( ( speed < 0 && currentHeight <= Constants.ELEVATOR_BOTTOM )
+				|| ( speed > 0 && currentHeight >= Constants.ELEVATOR_TOP ) )
+			speed = 0;
+		if (currentHeight <= Constants.ELEVATOR_BOTTOM) reset();
 		left.set(speed);
 		right.set(speed);
 	}
 	
-	public void setSpeed(double leftSpeed, double rightSpeed) {
-		left.set(leftSpeed);
-		right.set(rightSpeed);
+	/**
+	 * Function to go to height based
+	 * 
+	 * @param height - height in inches
+	 * 
+	 * @return true, when complete and stopped
+	 */
+	public boolean height(double height) {
+		double delta = height - height(); //Which way?
+		double speed = 0; //Default
+		elevStep = Math.abs(delta) <= Constants.ELEV_TOLERANCE; //Are we there yet?
+		if (!elevStep)
+			speed = delta / Constants.ELEVATOR_TOP; 
+		//Calculus, similar to Newton's Law of Cooling. Integrate this. 
+
+		setSpeed(speed);
+
+		return !elevStep;
 	}
 	
-	public boolean height(double height) {
-
-		double current = encoder.getDistance();
-		if ( Math.abs(height-current) <= Constants.ELEV_TOLERNCE ) {
-			setSpeed(0);
-			return true;
-		}
-		
-		double speed = 0.6 * Math.signum(height-current);
-		
-		setSpeed(speed);
-		
-		return false;
+	/**
+	 * @return The mast height, in inches.
+	 */
+	public double height() {
+		double current = height();
+		return current;
+		/*return current > Constants.ELEVATOR_HEIGHT_THRESHOLD
+				? 2 * current - Constants.ELEVATOR_HEIGHT_THRESHOLD
+				: current ;*/ //No idea how this works
 	}
 
 	private void reset() {
