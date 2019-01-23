4025e98ead8e9944ce795d675063e1a856e9bfc7
raiderrobotix25 <frcteam25@gmail.com>
2019-01-23 16:23:23 -0500

    Simplify elevator logic, add experimental elevator speed inversion

--- src/main/java/frc/robot/Elevator.java
+++ src/main/java/frc/robot/Elevator.java
@@ -8,16 +8,16 @@ public class Elevator {
 
   private static Elevator m_instance;
 
-  // private final CANSparkMax leftMotor;
-  // private final CANSparkMax rightMotor;
+  private final CANSparkMax leftMotor;
+  private final CANSparkMax rightMotor;
 
   // private final CANEncoder encoder;
 
   private Elevator() {
-    //this.leftMotor = new CANSparkMax(Constants.LEFT_ELEVATOR_CAN, MotorType.kBrushless);
-    //this.rightMotor = new CANSparkMax(Constants.RIGHT_ELEVATOR_CAN, MotorType.kBrushless);
+    this.leftMotor = new CANSparkMax(Constants.LEFT_ELEVATOR_CAN, MotorType.kBrushless);
+    this.rightMotor = new CANSparkMax(Constants.RIGHT_ELEVATOR_CAN, MotorType.kBrushless);
 
-    //this.encoder = this.leftMotor.getEncoder();
+    // this.encoder = this.leftMotor.getEncoder();
   }
 
   /**
@@ -31,21 +31,28 @@ public class Elevator {
     return m_instance;
   }
 
+  /**
+   * @param speed Elevator speed
+   * 
+   */
+
   public void setSpeed(double speed) {
-    // this.leftMotor.set(speed * (Constants.LEFT_ELEVATOR_INVERTED ? -1.0 : 1.0));
-    // this.rightMotor.set(speed * (Constants.RIGHT_ELEVATOR_INVERTED ? -1.0 : 1.0));
+
+    this.leftMotor.set(speed * (Constants.LEFT_ELEVATOR_INVERTED ? -1.0 : 1.0));
+    this.rightMotor.set(speed * (Constants.RIGHT_ELEVATOR_INVERTED ? -1.0 : 1.0));
   }
 
   /**
-   * @return The elevator height, in encoder ticks, scaled appropriately to account
-    for the trolley section on the last stage.
+   * @return The elevator height, in encoder ticks, scaled appropriately to
+   *         account for the trolley section on the last stage.
    */
   public double getHeight() {
     // return this.encoder.getPosition();
     return 0.0;
     // TODO: Update constants for new trolley position and use the below logic.
-    // return this.encoder.getPosition() > Constants.ELEVATOR_DOUBLE_HEIGHT_THRESHOLD
-    //     ? 2 * this.encoder.getPosition() - Constants.ELEVATOR_DOUBLE_HEIGHT_THRESHOLD
-    //     : this.encoder.getPosition();
+    // return this.encoder.getPosition() >
+    // Constants.ELEVATOR_DOUBLE_HEIGHT_THRESHOLD
+    // ? 2 * this.encoder.getPosition() - Constants.ELEVATOR_DOUBLE_HEIGHT_THRESHOLD
+    // : this.encoder.getPosition();
   }
 }
