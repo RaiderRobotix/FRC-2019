c976d08196d0355a4c440ba768e58ae5b2c17e93
K2SO <24651269+K2SO@users.noreply.github.com>
2019-02-10 12:34:38 -0500

    Move goToHeight functionality to a command

--- src/main/java/frc/robot/commands/Elevator/RaiseElevatorToHeight.java
+++ src/main/java/frc/robot/commands/Elevator/RaiseElevatorToHeight.java
@@ -8,21 +8,67 @@
 package frc.robot.commands.Elevator;
 
 import frc.robot.subsystems.Elevator;
+import frc.robot.Constants;
 
 import edu.wpi.first.wpilibj.command.Command;
 
 public class RaiseElevatorToHeight extends Command {
   
-  private Elevator elevator;
+  private static final Elevator elevator = Elevator.getInstance();
+  
+  private static final double UP_SCALED_RANGE_START = 20.0;
+  private static final double UP_SCALED_RANGE_END
+      = 0.5 * UP_SCALED_RANGE_START;
+  private static final double ALLOWED_DEVIATION = 1.0; // in inches
+  private static final double LOWER_LIMIT = 0.0;
+
+  
   private double targetHeight;
   private boolean isDone;
 
   public RaiseElevatorToHeight(double height) {
-    elevator = Elevator.getInstance();
+    
     requires(elevator);
 
     targetHeight = height;
   }
+  
+  private enum Range {
+		UP_FAR(0.9), 
+		UP_SCALED(0.7), // Just a default speed, is overwritten
+		UP_NEAR(0.5), 
+		AT_TARGET(0),
+		DOWN_NEAR(-0.3),
+		DOWN_FAR(-0.5);
+		
+		public final double speed;
+		
+		Range(double speed) {
+			this.speed = speed;
+		}
+		
+		private static Range getRange(double delta) {
+			Range range = Range.AT_TARGET;
+
+			if (delta < ALLOWED_DEVIATION ) {
+				if (delta > 0) {
+					if (delta > UP_SCALED_RANGE_START)
+						range = Range.UP_FAR;
+					else if (delta > UP_SCALED_RANGE_END)
+						range = Range.UP_SCALED;
+					else
+						range = Range.UP_NEAR;
+				} else
+					if (delta > -10.0)
+						range = Range.DOWN_NEAR;
+					else
+						range = Range.DOWN_FAR;
+			} else
+				range = Range.AT_TARGET;
+			
+			return range;
+		}
+	}
 
   // Called just before this Command runs the first time
   @Override
@@ -32,7 +78,23 @@ public class RaiseElevatorToHeight extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    isDone = elevator.goToHeight(targetHeight);
+    double currentHeight = elevator.getHeight();
+	double delta = targetHeight - currentHeight;
+	Range range = Range.getRange(delta);
+	double speed = range.speed;
+	
+	if (currentHeight < LOWER_LIMIT + ALLOWED_DEVIATION) {
+		elevator.resetEncoder();
+	}
+	
+	if (range==Range.UP_SCALED) {
+		double speedDelta = Constants.ELEVATOR_SCALE_START_SPEED - Constants.ELEVATOR_SCALE_END_SPEED;
+		speed = Constants.ELEVATOR_SCALE_END_SPEED + (speedDelta * (delta - Constants.ELEVATOR_UP_SCALED_RANGE_END)
+				/ (Constants.ELEVATOR_UP_SCALED_RANGE_START - Constants.ELEVATOR_UP_SCALED_RANGE_END));
+	}
+	
+	elevator.setSpeed(speed);
+	isDone = range == Range.AT_TARGET;
   }
 
   // Make this return true when this Command no longer needs to run execute()
