5ee262e7472414ae2aedf677805b75a66b35bead
raiderrobotix25 <frcteam25@gmail.com>
2019-02-16 19:20:17 -0500

    Add Turn Command

---
+++ src/main/java/frc/robot/commands/DriveBase/Turn.java
@@ -0,0 +1,64 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.DriveBase;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.DriveBase;
+
+public class Turn extends Command {
+
+  private final DriveBase drives;
+  private boolean isDone;
+  double targetAngle;
+  double speed;
+
+  public Turn(double angle, double speed) {
+    drives = DriveBase.getInstance();
+    requires(drives);
+
+    isDone = false;
+    this.targetAngle = angle;
+    this.speed = Math.copySign(speed, angle);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    isDone = false;
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    drives.setSpeed(speed, -speed);
+    if (Math.abs(drives.getGyroAngle() - targetAngle) < Constants.TURN_TOLERANCE) {
+      isDone = true;
+      drives.setSpeed(0.0);
+    }
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return isDone;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+    drives.setSpeed(0.0);
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+    end();
+  }
+}
